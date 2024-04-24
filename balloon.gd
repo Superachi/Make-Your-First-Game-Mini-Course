b06d8dfe4971a61df516072e2de465b648620cc1
PascalKoeimanHHS <psclkmn@gmail.com>
2024-04-24 09:58:16 +0200

    Add balloon hurtbox and make it so the balloon deletes itself on collision with sawblade

--- balloon.gd
+++ balloon.gd
@@ -1,7 +1,15 @@
 extends CharacterBody2D
+@onready var hurtbox = $Hurtbox
+
+# _ready() is called when the game starts up (runs once)
+func _ready():
+	hurtbox.body_entered.connect(_on_hurtbox_body_entered)
 
 # _physics_process() gets called every single physics step
 func _physics_process(delta):
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_vector * 100
 	move_and_slide()
+
+func _on_hurtbox_body_entered(body):
+	queue_free()
